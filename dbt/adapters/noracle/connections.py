2d169bd725e30cda4123f74bbfa01008063eebc3
emacha <15370927+emacha@users.noreply.github.com>
2021-03-21 20:05:00 +0000

    Implement exception_handler

--- dbt/adapters/noracle/connections.py
+++ dbt/adapters/noracle/connections.py
@@ -4,6 +4,17 @@ from contextlib import contextmanager
 from dbt.adapters.base import Credentials
 from dbt.adapters.sql import SQLConnectionManager
 from dbt.logger import GLOBAL_LOGGER as logger
+import cx_Oracle
+
+
+
+import debugpy
+
+
+# 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+debugpy.listen(5678)
+print("Waiting for debugger attach")
+#debugpy.wait_for_client()
 
 
 @dataclass
@@ -29,6 +40,7 @@ class NoracleConnectionManager(SQLConnectionManager):
 
     @classmethod
     def open(cls, connection):
+
         raise RuntimeError("Not fully implemented yet!")
     
     @classmethod
@@ -46,17 +58,16 @@ class NoracleConnectionManager(SQLConnectionManager):
         logger.debug("Canceled query '{}': {}".format(connection_name, res))
 
     @contextmanager
-    def exception_handler(self, sql: str):
-        raise RuntimeError("Not fully implemented yet!")
+    def exception_handler(self, sql: str):        
         try:
             yield
-        except myadapter_library.DatabaseError as exc:
-            self.release(connection_name)
+        except cx_Oracle.DatabaseError as exc:
+            self.release()
 
-            logger.debug('myadapter error: {}'.format(str(e)))
+            logger.debug(f'myadapter error: {str(exc)}')
             raise dbt.exceptions.DatabaseException(str(exc))
         except Exception as exc:
-            logger.debug("Error running SQL: {}".format(sql))
+            logger.debug(f"Error running SQL: {sql}")
             logger.debug("Rolling back transaction.")
-            self.release(connection_name)
+            self.release()
             raise dbt.exceptions.RuntimeException(str(exc))
