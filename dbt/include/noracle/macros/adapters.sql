5789d6951855d88524595aa5398947ba056d0b36
emacha <15370927+emacha@users.noreply.github.com>
2021-03-24 22:03:35 +0000

    Implement create_table_as macro

--- dbt/include/noracle/macros/adapters.sql
+++ dbt/include/noracle/macros/adapters.sql
@@ -110,3 +110,10 @@
 
   {{ return(load_result('list_schemas').table) }}
 {% endmacro %}
+
+{% macro noracle__create_table_as(temporary, relation, sql) -%}
+  create {% if temporary: -%}global temporary{%- endif %} table
+    {{ relation.include(database=false, schema=(not temporary)) }}
+  as 
+    {{ sql }}
+{% endmacro %}
