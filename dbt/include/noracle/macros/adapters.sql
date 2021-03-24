f88604ddbbb17271699b77c77d7f42300e942c9d
emacha <15370927+emacha@users.noreply.github.com>
2021-03-24 20:34:57 +0000

    Implement list_relations_without_caching

--- dbt/include/noracle/macros/adapters.sql
+++ dbt/include/noracle/macros/adapters.sql
@@ -1,31 +1,43 @@
 
 {% macro noracle__get_columns_in_relation(relation) -%}
-
   {% set msg -%}
     get_columns_in_relation not implemented for noracle
   {%- endset %}
-
   {{ exceptions.raise_compiler_error(msg) }}
-
 {% endmacro %}
 
 
 {% macro noracle__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True, auto_begin=False) -%}
+    select
+    '{{schema_relation.database}}' as database,
+    table_name as name,
+    '{{schema_relation.schema}}' as schema,
+    'table' as table_type
+    from all_tables
+    where owner = '{{schema_relation.schema}}'
 
-  {% set msg -%}
-    list_relations_without_caching not implemented for noracle
-  {%- endset %}
+    union all
 
-  {{ exceptions.raise_compiler_error(msg) }}
+    select
+        '{{schema_relation.database}}' as database,
+        view_name as name,
+        '{{schema_relation.schema}}' as schema,
+        'view' as table_type
+    from all_views
+    where owner = '{{schema_relation.schema}}'
+  {% endcall %}
+
+  {{ return(load_result('list_relations_without_caching').table) }}
 
 {% endmacro %}
 
 
 {% macro noracle__list_schemas(database) -%}
-  {% set sql %}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
     select distinct username as schema_name
     from sys.all_users
-  {% endset %}
-  
-  {{ return(run_query(sql)) }}
+  {% endcall %}
+
+  {{ return(load_result('list_schemas').table) }}
 {% endmacro %}
