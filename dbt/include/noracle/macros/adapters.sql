a414c5a4061c3cd739042f5cfa994d83aff1bfc2
emacha <15370927+emacha@users.noreply.github.com>
2021-03-24 23:45:33 +0000

    Implement create_view_as macro

--- dbt/include/noracle/macros/adapters.sql
+++ dbt/include/noracle/macros/adapters.sql
@@ -125,3 +125,8 @@
   as 
     {{ sql }}
 {% endmacro %}
+
+{% macro noracle__create_view_as(relation, sql) -%}
+  create view {{ relation.include(database=false, schema=true) }} as
+    {{ sql }}
+{% endmacro %}
