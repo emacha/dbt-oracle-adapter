8ce3c3028138cfbab844fb8ed89f30604c49d776
emacha <15370927+emacha@users.noreply.github.com>
2021-03-24 23:08:04 +0000

    Bugfix list_relations with a bit of a hack

--- dbt/adapters/noracle/impl.py
+++ dbt/adapters/noracle/impl.py
@@ -1,5 +1,6 @@
+from typing import Dict
 from dbt.adapters.sql import SQLAdapter
-
+from dbt.utils import filter_null_values
 from dbt.adapters.noracle import NoracleConnectionManager
 
 
@@ -12,3 +13,22 @@ class NoracleAdapter(SQLAdapter):
 
     def debug_query(self) -> None:
         self.execute("select 1 as id from dual")
+
+    def _make_match_kwargs(
+        self, database: str, schema: str, identifier: str
+    ) -> Dict[str, str]:
+        quoting = self.config.quoting
+        if identifier is not None and quoting['identifier'] is False:
+            identifier = identifier.upper()
+
+        if schema is not None and quoting['schema'] is False:
+            schema = schema.upper()
+
+        if database is not None and quoting['database'] is False:
+            database = database.upper()
+
+        return filter_null_values({
+            'database': database,
+            'identifier': identifier,
+            'schema': schema,
+        })
--- dbt/include/noracle/macros/adapters.sql
+++ dbt/include/noracle/macros/adapters.sql
@@ -61,11 +61,18 @@
   {{ exceptions.raise_compiler_error(msg) }}
 {% endmacro %}
 
-{% macro noracle__rename_relation(relation) -%}
-  {% set msg -%}
-    rename_relation not implemented for noracle
-  {%- endset %}
-  {{ exceptions.raise_compiler_error(msg) }}
+{% macro noracle__rename_relation(from_relation, to_relation) -%}
+  {% if from_relation.type == 'view' -%}
+    {% set msg -%}
+      rename view not implemented yet
+    {%- endset %}
+    {{ exceptions.raise_compiler_error(msg) }}
+  {% elif from_relation.type == 'table'%}
+    {% call statement('rename_relation') -%}
+      alter table {{from_relation.schema}}.{{from_relation.identifier}}
+      rename to {{to_relation.identifier}}
+    {%- endcall %}
+  {% endif %}
 {% endmacro %}
 
 {% macro noracle__truncate_relation(relation) -%}
@@ -79,22 +86,23 @@
 {% macro noracle__list_relations_without_caching(schema_relation) %}
   {% call statement('list_relations_without_caching', fetch_result=True, auto_begin=False) -%}
     select
-    '{{schema_relation.database}}' as database,
+    upper('{{schema_relation.database}}') as database,
     table_name as name,
-    '{{schema_relation.schema}}' as schema,
+    upper('{{schema_relation.schema}}') as schema,
     'table' as table_type
     from all_tables
-    where owner = '{{schema_relation.schema}}'
+    {# We're doing a case insensitive search in here! #}
+    where upper(owner) = upper('{{schema_relation.schema}}')
 
     union all
 
     select
-        '{{schema_relation.database}}' as database,
+        upper('{{schema_relation.database}}') as database,
         view_name as name,
-        '{{schema_relation.schema}}' as schema,
+        upper('{{schema_relation.schema}}') as schema,
         'view' as table_type
     from all_views
-    where owner = '{{schema_relation.schema}}'
+    where upper(owner) = upper('{{schema_relation.schema}}')
   {% endcall %}
 
   {{ return(load_result('list_relations_without_caching').table) }}
