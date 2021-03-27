9defaeeec522adaf51b9bdeb2694915a0436e57d
emacha <15370927+emacha@users.noreply.github.com>
2021-03-27 18:17:52 +0000

    Implement test_relationships macro for oracle

---
+++ dbt/include/noracle/macros/schema_tests.sql
@@ -0,0 +1,25 @@
+
+{% macro noracle__test_relationships(model, to, field) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('from')) %}
+
+
+{# This is tightly coupled to how the compiler injects `model` into the query. #}
+,
+child as (
+    select {{ column_name }} as id from {{ model }}
+),
+parent as (
+    select {{ field }} as id from {{ to }}
+),
+final as (
+    select *
+    from child
+    left join parent
+        on parent.id = child.id
+    where child.id is not null
+      and parent.id is null
+)
+select count(*) as validation_errors from final
+
+{% endmacro %}
